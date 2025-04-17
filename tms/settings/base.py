fe125ac291480bce3b47929edf5c08ac55edc309
John J Lawal <jophilip79@gmail.com>
2025-04-17 05:44:58 +0100

    Remove unused AWS storage settings from base.py

--- tms/settings/base.py
+++ tms/settings/base.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     "tms.apps.properties",
     "tms.apps.tenants",
 
-    "storages",
-
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
@@ -171,16 +169,6 @@ MEDIA_ROOT = BASE_DIR.parent.parent / "media"
 # ==============================================================================
 
 TMS_ENVIRONMENT = config("TMS_ENVIRONMENT", default="local")
-
-# AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-# AWS_S3_FILES_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# AWS_S3_SIGNATURE_VERSION = "s3v4"
-# AWS_S3_REGION_NAME = "us-east-2"
 
 # Custom Auth Settings
 AUTH_USER_MODEL = 'accounts.user'
